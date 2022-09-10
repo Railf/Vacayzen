1d7bc1f2a4a11cfc1caa268bed32038175accd41
Ralph McCracken <ralph@mccracken.work>
2022-09-09 23:28:59 -0500

    pull source data from google sheet and iterate through it to produce results files

--- GetiCalendarOccupancy.py
+++
@@ -1,2 +0,0 @@
-import ics
-import HTTPX
---
+++ GetiCalendars.py
@@ -0,0 +1,77 @@
+from ics import Calendar
+from pathlib import Path
+from glob import glob
+
+
+import credentials
+import webbrowser
+import requests
+import csv
+import os
+
+
+def WaitForDownload(filename, extension):
+    print("downloading occupancy sources... ", end="")
+
+    os.chdir(str(Path.home() / "Downloads"))
+
+    while not glob(filename + "*." + extension):
+        continue
+
+def GetCSVFromGoogleSheetTab(sheetID,tabID):
+    part0 = "https://docs.google.com/spreadsheets/d/"
+    part1 = sheetID
+    part2 = "/export?format=csv&gid="
+    part3 = tabID
+
+    webbrowser.open(part0 + part1 + part2 + part3)
+    WaitForDownload("iCalendar Converter - SOURCES","csv")
+    print("SUCCESS")
+
+
+def GetDataFromCSV():
+    data = {}
+    with open(Path.home() / "Downloads/iCalendar Converter - SOURCES.csv") as file:
+        reader = csv.DictReader(file)
+
+        data = list(reader)
+    
+    return data
+
+def CreateCSV(data, name):
+    filepath = "Downloads/"+name+".csv"
+
+    print("generating "+name+".csv...")
+
+    with open(str(Path.home() / filepath), 'w') as file:
+        write = csv.writer(file)
+        write.writerows(data)
+
+
+GetCSVFromGoogleSheetTab(credentials.GoogleSheetIDs["iCalendar Converter"],"1687799206")
+
+calendars = []
+calendars = GetDataFromCSV()
+
+while calendars == []: continue
+
+results = [["PARTNER","UNIT","ARRIVAL","DEPARTURE"]]
+failures = [["PARTNER","UNIT"]]
+
+for calendar in calendars:
+    try:
+        print("downloading", calendar["UNIT"], "occupancy... ", end="")
+
+        reservations = Calendar(requests.get(calendar["CALENDAR"]).text)
+
+        for reservation in reservations.events:
+            results.append([calendar["PARTNER"], calendar["UNIT"], reservation.begin.format("MM/DD/YYYY"), reservation.end.format("MM/DD/YYYY")])
+        
+        print("SUCCESS")
+    except:
+        print("FAILED")
+        failures.append([calendar["PARTNER"], calendar["UNIT"]])
+        continue
+
+CreateCSV(failures, "failures")
+CreateCSV(results,  "results")

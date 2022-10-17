183a12d181fd730a1a51a63c68b461c072aee52c
Ralph McCracken <ralph@mccracken.work>
2022-10-17 17:06:05 -0500

    added utilization percentage

--- InventoryUtilization.py
+++ InventoryUtilization.py
@@ -7,22 +7,33 @@ pd.options.mode.chained_assignment = None
 range_start = pd.to_datetime(sys.argv[1])
 range_end   = pd.to_datetime(sys.argv[2])
 
-df = pd.read_csv("/Users/workhorse/Downloads/dbo_vAvailability.csv", index_col=False)
-df = df[['RentalAssetMasterID','Quantity','StartDate','EndDate','AvailabilityType']]
+utilization = pd.read_csv("/Users/workhorse/Downloads/InventoryUtilization.csv", index_col=False)
 
-df.columns = ['asset','quantity','start','end','type']
+inventory = pd.read_csv("/Users/workhorse/Downloads/InventoryByItem.csv", index_col=False)
+inventory = dict(inventory.values)
 
-df['start']    = pd.to_datetime(df['start'])
-df['end']      = pd.to_datetime(df['end'])
+
+control = utilization[(utilization.RentalAssetMasterID == 236)].index
+utilization.drop(control,inplace=True)
+
+utilization = utilization[['Description','Quantity','StartDate','EndDate']]
+
+utilization.columns = ['asset','quantity','start','end']
+
+utilization['start']    = pd.to_datetime(utilization['start'])
+utilization['end']      = pd.to_datetime(utilization['end'])
 
 date_range = pd.date_range(range_start, range_end)
 
 inventory_by_date = []
 
 for day in date_range:
-    temp = df[(df.start <= day) & (df.end >= day)]
+    print(day.date())
+    temp = utilization[(utilization.start <= day) & (utilization.end >= day)]
     temp = temp.groupby('asset').sum()
     temp['date'] = day
+    temp['inventory'] = temp.index.map(inventory)
+    temp['utilization'] = temp['quantity'] / temp['inventory']
 
     inventory_by_date.append(temp)
 

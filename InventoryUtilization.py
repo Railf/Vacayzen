1db769b0ea6e016564880de41626407f8e1c81c7
Ralph McCracken <ralph@mccracken.work>
2022-10-24 17:18:23 -0500

    removal of fluff orders from analysis

--- InventoryUtilization.py
+++ InventoryUtilization.py
@@ -13,8 +13,8 @@ utilization = pd.read_csv("/Users/workhorse/Downloads/InventoryUtilization.csv",
 inventory = pd.read_csv("/Users/workhorse/Downloads/InventoryByItem.csv", index_col=False)
 inventory = dict(inventory.values)
 
-control = utilization[(utilization.RentalAssetMasterID == 236)].index
-utilization.drop(control,inplace=True)
+control = utilization['RentalAgreementID'][utilization.RentalAssetMasterID == 236].values
+utilization = utilization[utilization.RentalAgreementID.isin(control) == False]
 
 utilization = utilization[['Description','Quantity','StartDate','EndDate']]
 
@@ -90,26 +90,25 @@ utilization_by_bucket = pd.concat(buckets)
 
 utilization_by_bucket.to_csv('/Users/workhorse/Downloads/utilization_by_bucket.csv')
 
-# max_utilization_by_asset = []
+max_utilization_by_asset = []
 
-# for asset in utilization_by_date.index.unique():
-#     print(asset)
-#     temp = utilization_by_date[utilization_by_date.index == asset]
-#     temp = temp[temp.quantity == temp.quantity.max()]
+for asset in utilization_by_date.index.unique():
+    temp = utilization_by_date[utilization_by_date.index == asset]
+    temp = temp[temp.quantity == temp.quantity.max()]
 
-#     max_utilization_by_asset.append(temp)
+    max_utilization_by_asset.append(temp)
 
-# max_utilization = pd.concat(max_utilization_by_asset)
-# max_utilization.to_csv('/Users/workhorse/Downloads/max_utilization.csv')
+max_utilization = pd.concat(max_utilization_by_asset)
+max_utilization.to_csv('/Users/workhorse/Downloads/max_utilization.csv')
 
-# zero_to_fourty_nine = max_utilization[max_utilization.utilization < 0.5]
-# zero_to_fourty_nine.to_csv('/Users/workhorse/Downloads/max_1_zero_to_fourty_nine.csv')
+zero_to_fourty_nine = max_utilization[max_utilization.utilization < 0.5]
+zero_to_fourty_nine.to_csv('/Users/workhorse/Downloads/max_1_zero_to_fourty_nine.csv')
 
-# fifty_to_seventy_four = max_utilization[(max_utilization.utilization >= 0.5) & (max_utilization.utilization < 0.75)]
-# fifty_to_seventy_four.to_csv('/Users/workhorse/Downloads/max_2_fifty_to_seventy_four.csv')
+fifty_to_seventy_four = max_utilization[(max_utilization.utilization >= 0.5) & (max_utilization.utilization < 0.75)]
+fifty_to_seventy_four.to_csv('/Users/workhorse/Downloads/max_2_fifty_to_seventy_four.csv')
 
-# seventy_five_to_eighty_nine = max_utilization[(max_utilization.utilization >= 0.75) & (max_utilization.utilization < 0.9)]
-# seventy_five_to_eighty_nine.to_csv('/Users/workhorse/Downloads/max_3_seventy_five_to_eighty_nine.csv')
+seventy_five_to_eighty_nine = max_utilization[(max_utilization.utilization >= 0.75) & (max_utilization.utilization < 0.9)]
+seventy_five_to_eighty_nine.to_csv('/Users/workhorse/Downloads/max_3_seventy_five_to_eighty_nine.csv')
 
-# ninety_and_up = max_utilization[max_utilization.utilization >= 0.9]
-# ninety_and_up.to_csv('/Users/workhorse/Downloads/max_4_ninety_and_up.csv')
+ninety_and_up = max_utilization[max_utilization.utilization >= 0.9]
+ninety_and_up.to_csv('/Users/workhorse/Downloads/max_4_ninety_and_up.csv')

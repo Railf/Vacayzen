4cb6557547c101487e813640f6dc27b3263bc4da
Ralph McCracken <ralph@mccracken.work>
2022-11-30 20:31:20 -0600

    working for all categories

--- UnitsPerMonth.py
+++ UnitsPerMonth.py
@@ -1,7 +1,5 @@
 import pandas as pd
 
-import sys
-
 pd.options.mode.chained_assignment = None
 
 df = pd.read_csv("/Users/workhorse/Downloads/ppr.csv", index_col=False)
@@ -26,42 +24,38 @@ garts.columns    = ['partner','property','units','type','start','end']
 sets.columns     = ['partner','property','units','start','end']
 pelotons.columns = ['partner','property','units','start','end']
 
+date_range = pd.date_range("01/01/2022","10/31/2022",freq="M")
 
-bikes['start'] = pd.to_datetime(bikes['start'])
-bikes['end']   = pd.to_datetime(bikes['end'])
-bikes = bikes[bikes.start.isna() == False]
+categories = [bikes, garts, sets, pelotons]
 
+results = []
 
-# garts['start'] = pd.to_datetime(garts['start'])
-# garts['end']   = pd.to_datetime(garts['end'])
-# garts = garts[garts.start.isna() == False]
+for category in categories:
+    df = category
 
-# sets['start'] = pd.to_datetime(sets['start'])
-# sets['end']   = pd.to_datetime(sets['end'])
-# sets = sets[sets.start.isna() == False]
+    df['start'] = pd.to_datetime(df['start'])
+    df['end']   = pd.to_datetime(df['end'])
+    df = df[df.start.isna() == False]
 
-# pelotons['start'] = pd.to_datetime(pelotons['start'])
-# pelotons['end']   = pd.to_datetime(pelotons['end'])
-# pelotons = pelotons[pelotons.start.isna() == False]
+    year = []
 
-date_range = pd.date_range("01/01/2022","12/31/2022",freq="M")
+    for date in date_range:
+        month = df[(df.start <= date) & ((df.end > date) | (df.end.isna()))]
+        month = month.groupby(['partner']).sum(True)
+        month.columns = [date]
+        year.append(month)
 
-year = []
+    result = pd.concat(year, axis=1)
+    results.append(result)
 
-df = bikes
+for i, result in enumerate(results):
+    name = ''
 
-for date in date_range:
-    month = df[(df.start <= date) & ((df.end > date) | (df.end.isna()))]
-    month = month.groupby(['partner']).sum(True)
-    month.columns = [date]
-    year.append(month)
+    if   i == 0: name = 'bikes'
+    elif i == 1: name = 'garts'
+    elif i == 2: name = 'sets'
+    elif i == 3: name = 'pelotons'
 
-results = pd.concat(year, axis=1)
-
-results.to_csv('/Users/workhorse/Downloads/_bikes.csv')
-
-
-# bikes.to_csv('/Users/workhorse/Downloads/_bikes.csv')
-# garts.to_csv('/Users/workhorse/Downloads/_garts.csv')
-# sets.to_csv('/Users/workhorse/Downloads/_sets.csv')
-# pelotons.to_csv('/Users/workhorse/Downloads/_pelotons.csv')
+    file = '/Users/workhorse/Downloads/' + name + '.csv'
+    
+    result.to_csv(file)

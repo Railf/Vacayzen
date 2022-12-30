712dfe4cfd7d3d61072d853d1485f6056b14bf22
Ralph McCracken <ralph@mccracken.work>
2022-12-30 16:26:48 -0600

    initial commit

---
+++ AssetUtilization.py
@@ -0,0 +1,55 @@
+import pandas as pd
+
+import sys
+
+pd.options.mode.chained_assignment = None
+
+range_start = pd.to_datetime(sys.argv[1] + ' 18:00:00')
+range_end   = pd.to_datetime(sys.argv[2] + ' 10:00:00')
+
+df = pd.read_csv("/Users/workhorse/Downloads/AssetUtilization.csv")
+
+## FIELDS
+#  ID
+#  CustomerNumber                       # != 10331
+#  RentalStage                          # != 'Cancel'
+#  RentalAgreementTypeID                # != 3
+#  RentalAssetItemID                    # != 341, 342 
+#  RentalAssetMasterID                  # != 235, 236
+#  Description
+#  Quantity
+#  RentalAgreementReservationStartDate
+#  RentalAgreementReservationEndDate
+
+filters = []
+
+filters.append(list(set(df.ID[df.CustomerNumber == 10331].values)))
+filters.append(list(set(df.ID[df.RentalStage == 'Cancel'].values)))
+filters.append(list(set(df.ID[df.RentalAgreementTypeID == 3].values)))
+filters.append(list(set(df.ID[df.RentalAssetItemID == 341].values)))
+filters.append(list(set(df.ID[df.RentalAssetItemID == 342].values)))
+filters.append(list(set(df.ID[df.RentalAssetMasterID == 235].values)))
+filters.append(list(set(df.ID[df.RentalAssetMasterID == 236].values)))
+
+for filter in filters: df = df[df.ID.isin(filter) == False]
+
+df          = df[['Description','Quantity','RentalAgreementReservationStartDate','RentalAgreementReservationEndDate']]
+df.columns  = ['asset','quantity','start','end']
+df          = df.reset_index()
+
+df['start'] = pd.to_datetime(df['start'])
+df['end']   = pd.to_datetime(df['end'])
+
+date_range  = pd.date_range(range_start, range_end)
+
+inventory_by_date = []
+
+for day in date_range:
+    temp = df[(df.start <= day) & (df.end >= day)]
+    temp = temp.groupby('asset').sum(numeric_only=True)
+    temp['date'] = day
+
+    inventory_by_date.append(temp)
+
+utilization = pd.concat(inventory_by_date)
+utilization.to_csv('/Users/workhorse/Downloads/utilization_by_date.csv')

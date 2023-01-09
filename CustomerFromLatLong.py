e2fe1a5bec9a704c21bc077abfa9ef9431e2bfa8
Ralph McCracken <ralph@mccracken.work>
2023-01-09 16:53:05 -0600

    saving...

--- CustomerFromLatLong.py
+++ CustomerFromLatLong.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
-df     = pd.read_csv("/Users/workhorse/Downloads/CustomerFromLatLong.csv")
+df       = pd.read_csv("/Users/workhorse/Downloads/CustomerFromLatLong.csv")
 partners = pd.read_csv("/Users/workhorse/Downloads/partners.csv")
 
 
-df = df.loc[df['CustomerNumber'].isin(partners.CID.values)]
+df  = df.loc[df['CustomerNumber'].isin(partners.CID.values)]
 
 out = df.merge(partners, left_on='CustomerNumber', right_on='CID').reindex(columns=['CID','Customer','RentalAgreementReservationEndDate','AgreementTotal'])
 

f262be4fe5851d2113ee1dc7151fbf80873ece5b
Ralph McCracken <ralph@mccracken.work>
2023-01-12 16:58:46 -0600

    initial commit

---
+++ GetLicensePlateData.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+
+import csv
+
+
+def GetDataFromCSV():
+    data = {}
+    with open(Path.home() / "Downloads/plates.csv") as file:
+        reader = csv.DictReader(file)
+
+        data = list(reader)
+    
+    return data
+
+
+def CreateCSV(name, data):
+    print("generating", name + ".csv...")
+
+    namePath = "Downloads/" + name + ".csv"
+
+    filename = str(Path.home() / namePath)
+
+    with open(filename, 'w') as file:
+        write = csv.writer(file)
+        write.writerows(data)
+
+
+properties = GetDataFromCSV()
+counts     = []
+results    = []
+
+for index, property in enumerate(properties):
+    counts.append(int(property['Count']))
+
+for index, property in enumerate(properties):
+    for x in range(counts[index]):
+        results.append([property['Name'],property['Address']])
+
+CreateCSV("results", results)

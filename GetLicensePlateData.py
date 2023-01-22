40067e3bb8ca5d26696944000ffb7cb9303f9562
Ralph McCracken <ralph@mccracken.work>
2023-01-21 23:39:41 -0600

    saving...

--- GetLicensePlateData.py
+++ GetLicensePlateData.py
@@ -17,9 +17,9 @@ def GetDictFromCSV(file):
 
 
 def CreateCSV(name, data):
-    print("generating", name + ".csv...")
+    print('generating', name + '.csv...')
 
-    namePath = "Downloads/" + name + ".csv"
+    namePath = 'Downloads/' + name + '.csv'
 
     filename = str(Path.home() / namePath)
 
@@ -28,7 +28,7 @@ def CreateCSV(name, data):
         write.writerows(data)
 
 
-properties  = GetDictFromCSV("plates.csv")
+properties  = GetDictFromCSV('plates.csv')
 houseName   = []
 noHouseName = []
 plateCount  = 0
@@ -42,22 +42,22 @@ for index, property in enumerate(properties):
             houseName.append([index, property['Name'], property['Address']])
 
 for index, property in enumerate(houseName):
-    property[0] = "HOUSE-" + str(index)
+    property[0] = 'HOUSE-' + str(index)
+    # TODO: Remove " from house names
 
 for index, property in enumerate(noHouseName):
-    property[0] = "NOHOUSE-" + str(index)
-
+    property[0] = 'NOHOUSE-' + str(index)
 
 platesWithNames   = len(houseName)
 platesWithNoNames = len(noHouseName)
 
-if platesWithNames > 0: CreateCSV("house", houseName)
-if platesWithNoNames > 0: CreateCSV("no house", noHouseName)
+if platesWithNames > 0:   CreateCSV('house',    houseName)
+if platesWithNoNames > 0: CreateCSV('no house', noHouseName)
 
-print("")
-print("plates requested in file:            ", plateCount)
-print("plates for homes with house name:    ", platesWithNames)
-print("plates for homes without house name: ", platesWithNoNames)
-print("plates to be printed:                ", platesWithNames + platesWithNoNames)
-print("")
-print("plates requested match to-be-printed?", str(plateCount == (platesWithNames + platesWithNoNames)).lower())
+print('')
+print('plates requested in file:            ', plateCount)
+print('plates for homes with house name:    ', platesWithNames)
+print('plates for homes without house name: ', platesWithNoNames)
+print('plates to be printed:                ', platesWithNames + platesWithNoNames)
+print('')
+print('plates requested match to-be-printed?', str(plateCount == (platesWithNames + platesWithNoNames)).lower())

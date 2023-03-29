38628ebdede1f1fe83d6af5961c6d40167ec3f79
Ralph McCracken <ralph@mccracken.work>
2023-03-29 17:04:26 -0500

    saving

--- SubmitToBeachy.py
+++ SubmitToBeachy.py
@@ -28,6 +28,14 @@ def GetDataFromCSV():
 def NavigateToNewOrderScreen():
     driver.get(credentials.beachy_Vacayzen["url"])
 
+def NavigateToDestinationItem(destination, item):
+    ClickAtID("select2-destination-select-container")
+    ProvideTextToXPATH(destination + Keys.RETURN,"/html/body/span/span/span[1]/input")
+    time.sleep(0.2)
+    ClickAtID("select2-product-select-container")
+    ProvideTextToXPATH(item + Keys.RETURN,"/html/body/span/span/span[1]/input")
+    time.sleep(0.2)
+
 def ProvideTextToID(text, id):
     driver.find_element(By.ID,id).send_keys(text)
 
@@ -47,12 +55,26 @@ def Login():
     ClickAtID("button_sign_in")
 
 def SubmitOrder():
+    startDate = "03/01/2023"
+    endDate   = "03/04/2023"
+    quantity  = "2"
+
+
     NavigateToNewOrderScreen()
-    ClickAtID("select2-destination-select-container")
-    ProvideTextToXPATH("Carillon Beach" + Keys.RETURN,"/html/body/span/span/span[1]/input")
+    NavigateToDestinationItem("Carillon Beach", "Seasonal Sets")
+    ProvideTextToID(startDate, "start_date")
+    ProvideTextToID(endDate, "end_date")
     time.sleep(0.2)
-    ClickAtID("select2-product-select-container")
-    ProvideTextToXPATH("Seasonal Sets"  + Keys.RETURN,"/html/body/span/span/span[1]/input")
+    ProvideTextToXPATH(quantity, "/html/body/div[2]/div[2]/div/div[2]/div[1]/form/div[2]/div/div[2]/div[2]/div[2]/input")
+
+
+
+
+    # ClickAtXPATH("/html/body/div[2]/div[2]/div/div[2]/div[1]/form/div[2]/div/div[3]/div[2]/div[1]/a")
+    # time.sleep(0.2)
+    # ClickAtID("add-to-cart")
+    # time.sleep(0.2)
+    # ClickAtID("cart-checkout-btn")
 
 
 Login()

c335f10f9074bf908fa05eb4f84cda9d524c58f5
Ralph McCracken <ralph@mccracken.work>
2023-04-17 15:54:48 -0500

    different approach

--- CustomerFromLatLong.py
+++ CustomerFromLatLong.py
@@ -1,11 +1,16 @@
 import pandas as pd
 
-df       = pd.read_csv("/Users/workhorse/Downloads/CustomerFromLatLong.csv")
 partners = pd.read_csv("/Users/workhorse/Downloads/partners.csv")
+erp      = pd.read_excel("/Users/workhorse/Downloads/CTLL_ERPCustomer.xlsx")
+cst      = pd.read_excel("/Users/workhorse/Downloads/CTLL_CustomerShipTo.xlsx")
+ras      = pd.read_excel("/Users/workhorse/Downloads/CTLL_RentalAgreement.xlsx")
 
+print(cst.describe)
+cst = cst[cst.Status == 1]
+print(cst.describe)
 
-df  = df.loc[df['CustomerNumber'].isin(partners.CID.values)]
+# df = pd.merge(erp, cst,      left_on="ID",                     right_on="ERPCustomerID")
+# df = pd.merge(df,  partners, left_on="CustomerNumber",         right_on="CID")
+# df = pd.merge(df,  ras,      left_on=["Latitude","Longitude"], right_on=["AgrmtJobAddrLat","AgrmtJobAddrLong"])
 
-out = df.merge(partners, left_on='CustomerNumber', right_on='CID').reindex(columns=['CID','Customer','RentalAgreementNo','PaymentDate','TransactionAmount'])
-
-out.to_csv("/Users/workhorse/Downloads/channel_partners.csv")
+# print(df)

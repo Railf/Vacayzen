74b51e50355ae74248bff139035d2232461b447a
Ralph McCracken <ralph@mccracken.work>
2023-06-26 12:55:57 -0500

    in working order

--- GetRented.py
+++ GetRented.py
@@ -7,22 +7,51 @@ date = pd.to_datetime(sys.argv[1])
 
 print('reading in availability...')
 df          = pd.read_excel('/Users/workhorse/Downloads/Availability.xlsx')
-df.columns  = ['status','start','end','asset','quantity']
+df.columns  = ['start','end','asset','quantity']
 
 print('converting dates...')
 df['start'] = pd.to_datetime(df['start']).dt.normalize()
 df['end']   = pd.to_datetime(df['end']).dt.normalize()
 
-print('removing cancelled line items...')
-df = df[df['status'].isna() == True]
-df = df[['start','end','asset','quantity']]
-
 print('grabbing lines that include provided date...')
 df = df[(df['start'] <= date) & (df['end'] > date)]
 
 print('pivoting...')
-result = df.pivot_table(values=['quantity'], index=['asset'], aggfunc=np.sum)
-result = result.reset_index()
+rented = df.pivot_table(values=['quantity'], index=['asset'], aggfunc=np.sum)
+rented = rented.reset_index()
 
-print('generating rented.csv...')
-result.to_csv('/Users/workhorse/Downloads/rented.csv', index=False)
+
+
+print('reading in counted...')
+df          = pd.read_csv('/Users/workhorse/Downloads/Counted.csv')
+df.columns  = ['category','asset','quantity']
+
+print('merging rented and counted...')
+total           = pd.merge(df, rented, how='left', on='asset')
+total.columns   = ['category','asset','counted','rented']
+
+print('determing total, buffer, and final quantities...')
+total           = total.fillna(0)
+total['total']  = total.counted + total.rented
+total['buffer'] = total.total * 0.05
+total           = total.round(0)
+total['final']  = total.total - total.buffer
+
+
+
+print('reading in inventory...')
+df          = pd.read_excel('/Users/workhorse/Downloads/Inventory.xlsx')
+df.columns  = ['category','asset','current']
+df          = df[['asset','current']]
+
+print('merging total and inventory...')
+delta          = pd.merge(total, df, how='left', on='asset')
+delta['delta'] = delta.current - delta.final
+
+
+
+print('generating delta_detail.csv...')
+delta.to_csv('/Users/workhorse/Downloads/delta_detail.csv', index=False)
+
+print('generating delta.csv...')
+delta[['asset','delta']].to_csv('/Users/workhorse/Downloads/delta.csv', index=False)

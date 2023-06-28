c5befc62dc5e5b68538cffa8f546fa26e63413cd
Ralph McCracken <ralph@mccracken.work>
2023-06-28 18:54:46 -0500

    initial commit - in working order

---
+++ SWBSASetsPerDay.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+
+# import sys
+
+# date = pd.to_datetime(sys.argv[1])
+
+print('reading in availability...')
+df = pd.read_csv('/Users/workhorse/Downloads/Export_ExportRentalsByDay.csv', index_col=False)
+df = df[['Description','Quantity','RentalAgreementStartDate', 'RentalAgreementEndDate']]
+df.columns = ['access','sets','start','end']
+
+
+print('converting dates...')
+df['start'] = pd.to_datetime(df['start']).dt.normalize()
+df['end']   = pd.to_datetime(df['end']).dt.normalize()
+
+dates = pd.date_range(start='03/01/2023', end='10/31/2023')
+setsperday  = []
+
+for date in dates:
+    sets = df[(df['start'] <= date) & (df['end'] >= date)]
+    sets.columns = ['access',date,'start','end']
+    sets = sets.pivot_table(values=[date], index=['access'], aggfunc=np.sum)
+
+    setsperday.append(sets)
+
+result = pd.concat(setsperday, axis = 1)
+result = result.fillna(0)
+
+
+print('generating sets.csv...')
+result.to_csv('/Users/workhorse/Downloads/SetsPerAccessPerDay.csv')
